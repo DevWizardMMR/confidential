daaafd36bf1d6aed78f8e7eea8da23d8fd966f80
Mohaiminur-MernStackDeveloper <mohaiminur24@gmail.com>
2024-01-28 18:29:42 +0600

    blood donors and bank

---
+++ BloodBank.js
@@ -0,0 +1,159 @@
+[
+  {
+    name: "Red Crescent Blood Bank",
+    address: "7/5, Aurongzeb Road, Mohammadpur, Dhaka.",
+    district: "Dhaka",
+    contactNo: ["02 41023562", "02 223310368", "01811 458537"],
+    image:
+      "https://scontent.fdac22-1.fna.fbcdn.net/v/t39.30808-6/305491441_547197243874338_6756139105831412863_n.png?_nc_cat=106&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeEmseg1UA4aN7aRXpDA55tQ_VVz5_57NJf9VXPn_ns0l2-0uul5IhRq0gHQOwsZjoO1zX3An0U3WuCrvUmIlrDs&_nc_ohc=9Qh2otAtB0gAX991j9c&_nc_ht=scontent.fdac22-1.fna&oh=00_AfDN2ATxM_RRYDqJOLkv5CcgUVJ3MjfA2-Rz12BHheblYQ&oe=65BB1FD7",
+  },
+  {
+    name: "Fatema Begum Red Crescent Blood Center",
+    address: "395 Anderkilla, Chattogram",
+    district: "Chattogram",
+    contactNo: ["031 620926", "01815 850533", "01819 353445"],
+    image:
+      "https://scontent.fdac22-1.fna.fbcdn.net/v/t39.30808-6/305491441_547197243874338_6756139105831412863_n.png?_nc_cat=106&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeEmseg1UA4aN7aRXpDA55tQ_VVz5_57NJf9VXPn_ns0l2-0uul5IhRq0gHQOwsZjoO1zX3An0U3WuCrvUmIlrDs&_nc_ohc=9Qh2otAtB0gAX991j9c&_nc_ht=scontent.fdac22-1.fna&oh=00_AfDN2ATxM_RRYDqJOLkv5CcgUVJ3MjfA2-Rz12BHheblYQ&oe=65BB1FD7",
+  },
+  {
+    name: "Holy Family Red Crescent Blood Center",
+    address: "1st Floor, Out Door, 1 Eskaton Garden Road, Dhaka",
+    district: "Dhaka",
+    contactNo: ["01811 458536"],
+    image:
+      "https://scontent.fdac22-1.fna.fbcdn.net/v/t39.30808-6/305491441_547197243874338_6756139105831412863_n.png?_nc_cat=106&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeEmseg1UA4aN7aRXpDA55tQ_VVz5_57NJf9VXPn_ns0l2-0uul5IhRq0gHQOwsZjoO1zX3An0U3WuCrvUmIlrDs&_nc_ohc=9Qh2otAtB0gAX991j9c&_nc_ht=scontent.fdac22-1.fna&oh=00_AfDN2ATxM_RRYDqJOLkv5CcgUVJ3MjfA2-Rz12BHheblYQ&oe=65BB1FD7",
+  },
+  {
+    name: "Ahad Red Crescent Blood Center",
+    address: "Munshi Mehabullah Road, Jashore",
+    district: "Jashore",
+    contactNo: ["0421 68882", "01718 802794", "01939 109722"],
+    image:
+      "https://scontent.fdac22-1.fna.fbcdn.net/v/t39.30808-6/305491441_547197243874338_6756139105831412863_n.png?_nc_cat=106&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeEmseg1UA4aN7aRXpDA55tQ_VVz5_57NJf9VXPn_ns0l2-0uul5IhRq0gHQOwsZjoO1zX3An0U3WuCrvUmIlrDs&_nc_ohc=9Qh2otAtB0gAX991j9c&_nc_ht=scontent.fdac22-1.fna&oh=00_AfDN2ATxM_RRYDqJOLkv5CcgUVJ3MjfA2-Rz12BHheblYQ&oe=65BB1FD7",
+  },
+  {
+    name: "Begum Tayeeba Mojumder Red Crescent Blood Center",
+    address: "1 New Town, Dinajpur",
+    district: "Dinajpur",
+    contactNo: ["0531 61300", "01723 595972", "01717 184539"],
+    image:
+      "https://scontent.fdac22-1.fna.fbcdn.net/v/t39.30808-6/305491441_547197243874338_6756139105831412863_n.png?_nc_cat=106&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeEmseg1UA4aN7aRXpDA55tQ_VVz5_57NJf9VXPn_ns0l2-0uul5IhRq0gHQOwsZjoO1zX3An0U3WuCrvUmIlrDs&_nc_ohc=9Qh2otAtB0gAX991j9c&_nc_ht=scontent.fdac22-1.fna&oh=00_AfDN2ATxM_RRYDqJOLkv5CcgUVJ3MjfA2-Rz12BHheblYQ&oe=65BB1FD7",
+  },
+  {
+    name: "Mujib Jahan Red Crescent Blood Center",
+    address: "Chowhatta, Sylhet",
+    district: "Sylhet",
+    contactNo: ["01611 300900", "0821 724098"],
+    image:
+      "https://scontent.fdac22-1.fna.fbcdn.net/v/t39.30808-6/305491441_547197243874338_6756139105831412863_n.png?_nc_cat=106&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeEmseg1UA4aN7aRXpDA55tQ_VVz5_57NJf9VXPn_ns0l2-0uul5IhRq0gHQOwsZjoO1zX3An0U3WuCrvUmIlrDs&_nc_ohc=9Qh2otAtB0gAX991j9c&_nc_ht=scontent.fdac22-1.fna&oh=00_AfDN2ATxM_RRYDqJOLkv5CcgUVJ3MjfA2-Rz12BHheblYQ&oe=65BB1FD7",
+  },
+  {
+    name: "Natore Red Crescent Blood Center",
+    address: "Hospital Road, Natore",
+    district: "Natore",
+    contactNo: ["01850 124225", "0771 66961"],
+    image:
+      "https://scontent.fdac22-1.fna.fbcdn.net/v/t39.30808-6/305491441_547197243874338_6756139105831412863_n.png?_nc_cat=106&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeEmseg1UA4aN7aRXpDA55tQ_VVz5_57NJf9VXPn_ns0l2-0uul5IhRq0gHQOwsZjoO1zX3An0U3WuCrvUmIlrDs&_nc_ohc=9Qh2otAtB0gAX991j9c&_nc_ht=scontent.fdac22-1.fna&oh=00_AfDN2ATxM_RRYDqJOLkv5CcgUVJ3MjfA2-Rz12BHheblYQ&oe=65BB1FD7",
+  },
+  {
+    name: "Rajshahi Red Crescent Blood Center",
+    address: "Rajshahi District Road, Rajshahi",
+    district: "Rajshahi",
+    contactNo: ["01865 055075", "01556 333821"],
+    image:
+      "https://scontent.fdac22-1.fna.fbcdn.net/v/t39.30808-6/305491441_547197243874338_6756139105831412863_n.png?_nc_cat=106&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeEmseg1UA4aN7aRXpDA55tQ_VVz5_57NJf9VXPn_ns0l2-0uul5IhRq0gHQOwsZjoO1zX3An0U3WuCrvUmIlrDs&_nc_ohc=9Qh2otAtB0gAX991j9c&_nc_ht=scontent.fdac22-1.fna&oh=00_AfDN2ATxM_RRYDqJOLkv5CcgUVJ3MjfA2-Rz12BHheblYQ&oe=65BB1FD7",
+  },
+  {
+    name: "Achia Khatun Memorial Red Crescent Blood Center",
+    address: "Jhenidah Road, Stadium Para, Magura",
+    district: "Magura",
+    contactNo: ["01941 989197", "01941 989198", "01680 547365"],
+    image:
+      "https://scontent.fdac22-1.fna.fbcdn.net/v/t39.30808-6/305491441_547197243874338_6756139105831412863_n.png?_nc_cat=106&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeEmseg1UA4aN7aRXpDA55tQ_VVz5_57NJf9VXPn_ns0l2-0uul5IhRq0gHQOwsZjoO1zX3An0U3WuCrvUmIlrDs&_nc_ohc=9Qh2otAtB0gAX991j9c&_nc_ht=scontent.fdac22-1.fna&oh=00_AfDN2ATxM_RRYDqJOLkv5CcgUVJ3MjfA2-Rz12BHheblYQ&oe=65BB1FD7",
+  },
+  {
+    name: "Quantum Blood Bank Lab",
+    address: "31/V Shilpacharya Zainul Abedin Sarak, Shantinagar, Dhaka 1217",
+    district: "Dhaka",
+    contactNo: ["01714-010869"],
+    image: "path/to/quantum_blood_bank_lab_image.jpg",
+  },
+  {
+    name: "Alif Blood Bank & Transfusion Center",
+    address:
+      "44/11, West Panthapath (2nd Floor), Opposite of Shamrita Hospital, Dhaka 1215",
+    district: "Dhaka",
+    contactNo: ["01712392923", "01913059375"],
+    image: "path/to/alif_blood_bank_image.jpg",
+  },
+  {
+    name: "Badhan Blood Bank",
+    address:
+      "Central Office, T.S.C(Ground Floor), University of Dhaka, Dhaka-1000",
+    district: "Dhaka",
+    contactNo: ["02-8629042", "01534982674"],
+    image: "path/to/badhan_blood_bank_image.jpg",
+  },
+  {
+    name: "Thalassemia Blood Bank",
+    address: "30 Chamelibag, 1st Lane, Dhaka 1217",
+    district: "Dhaka",
+    contactNo: ["02-8332481"],
+    image: "path/to/thalassemia_blood_bank_image.jpg",
+  },
+  {
+    name: "Sandhani (DMC)",
+    address: "Dhaka Medical College Unit, Dhaka.",
+    district: "Dhaka",
+    contactNo: [
+      "+880-2-9668609",
+      "+880-2-9668690",
+      "+880-2-8616744",
+      "+880-2-9663429",
+    ],
+    mobile: "01819-284878",
+    image: "path/to/sandhani_dmc_image.jpg",
+  },
+  {
+    name: "Police Blood Bank",
+    address: "Central Police Hospital, Rajarbag, Dhaka.",
+    district: "Dhaka",
+    contactNo: ["9362573", "01713-398386"],
+    image: "path/to/police_blood_bank_image.jpg",
+  },
+  {
+    name: "Oriental Blood Bank",
+    address: "Green Center, 2B/30, Green Road, Dhanmondi, Dhaka",
+    district: "Dhaka",
+    contactNo: ["01812700053"],
+    image: "path/to/oriental_blood_bank_image.jpg",
+  },
+  {
+    name: "Mukti Blood Bank & Pathology Lab",
+    address:
+      "54 (1st Floor), Bir-Uttam A.M. Shafiullah Road, Free School Street, Dhaka-1207",
+    district: "Dhaka",
+    contactNo: ["+880 2-8624249"],
+    image: "path/to/mukti_blood_bank_image.jpg",
+  },
+  {
+    name: "Islami Bank Hospital Blood Bank",
+    address: "Dhaka, Bangladesh",
+    district: "Dhaka",
+    contactNo: ["02-8317090", "8321495"],
+    image: "path/to/islami_bank_hospital_blood_bank_image.jpg",
+  },
+  {
+    name: "New Bangladesh Pathology & Blood Bank",
+    address: "House # 69, Road # 9/A, Dhanmondi R/A, Dhaka-1209",
+    district: "Dhaka",
+    contactNo: ["+8801715546807", "+8801736877303"],
+    image: "path/to/new_bangladesh_pathology_blood_bank_image.jpg",
+  },
+  {
+    name: "Ideal Foundation (Diagnostic & Blood Bank)",
+    district: "Rangpur",
+    contactNo: ["01988-877800"],
+    image: "path/to/ideal_foundation_blood_bank_image.jpg",
+  },
+];
